ffcf99689890c76d30483d50329e87cf3b603f5e
root <root@Khadetou.localdomain>
2022-03-26 23:05:24 +0000

    configschema

---
+++ backend/src/config.schema.ts
@@ -0,0 +1,9 @@
+import * as Joi from 'joi';
+
+export const configValidationSchema = Joi.object({
+  STAGE: Joi.string().valid('dev', 'prod').required(),
+  MONGO_URI: Joi.string().required(),
+  JWT_SECRET: Joi.string().required(),
+  GOOGLE_SECRET: Joi.string().required(),
+  GOOGLE_CLIENT_ID: Joi.string().required(),
+});
