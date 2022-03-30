a018c270b8366eed76486a05206a502dca949319
khadetou <khadetou96@gmail.com>
2022-03-30 18:58:16 +0000

    cloudinary env fixes

--- backend/src/cloudinary/cloudinary.provider.ts
+++ backend/src/cloudinary/cloudinary.provider.ts
@@ -1,14 +1,16 @@
-import { ConfigService } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { v2 } from 'cloudinary';
 import { CLOUDINARY } from './constants';
 
 export const CloudinaryProvider = {
   provide: CLOUDINARY,
+  imports: [ConfigModule],
+  inject: [ConfigService],
   useFactory: async (configService: ConfigService) => {
     const cloudinary = v2.config({
-      cloud_name: 'didh3wbru',
-      api_key: '299593682714689',
-      api_secret: '5SLVYG27QnmLgYjHKJjtyhMcZ5k',
+      cloud_name: configService.get('CLOUDINARY_CLOUD_NAME'),
+      api_key: configService.get('CLOUDINARY_API_KEY'),
+      api_secret: configService.get('CLOUDINARY_API_SECRET'),
     });
     return cloudinary;
   },
