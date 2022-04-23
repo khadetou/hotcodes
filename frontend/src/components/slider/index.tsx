8f30043bd7cdc3207ee7ac6e6d1db828ffccbd0b
khadetou <khadetou96@gmail.com>
2022-04-23 07:31:30 +0000

    slider completed

--- frontend/src/components/slider/index.tsx
+++ frontend/src/components/slider/index.tsx
@@ -1,65 +1,62 @@
-import SmallCards from '../smallcards';
-import { useTranslation } from 'next-i18next';
-import Goal from '/public/images/process/goal.svg';
-import Design from '/public/images/process/design.svg';
-import Developement from '/public/images/process/production.svg';
-import arrow from '/public/images/swiper/arrow-right-circle.svg';
-import Result from '/public/images/process/result.svg';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import SmallCards from "../smallcards";
+import { useTranslation } from "next-i18next";
+import Goal from "/public/images/process/goal.svg";
+import Design from "/public/images/process/design.svg";
+import Developement from "/public/images/process/production.svg";
+import arrow from "/public/images/swiper/arrow-right-circle.svg";
+import Result from "/public/images/process/result.svg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Scrollbar, A11y } from "swiper";
 
 const Slider = () => {
-	const { t } = useTranslation('common');
-	console.log(arrow);
+  const { t } = useTranslation("common");
+  console.log(arrow);
 
-	const slides = [
-		{
-			title: t('process.cards.1.title'),
-			text: t('process.cards.1.p'),
-			img: Goal
-		},
-		{
-			title: t('process.cards.2.title'),
-			text: t('process.cards.2.p'),
-			img: Design
-		},
-		{
-			title: t('process.cards.3.title'),
-			text: t('process.cards.3.p'),
-			img: Developement
-		},
-		{
-			title: t('process.cards.4.title'),
-			text: t('process.cards.4.p'),
-			img: Result
-		}
-	];
+  const slides = [
+    {
+      title: t("process.cards.1.title"),
+      text: t("process.cards.1.p"),
+      img: Goal,
+    },
+    {
+      title: t("process.cards.2.title"),
+      text: t("process.cards.2.p"),
+      img: Design,
+    },
+    {
+      title: t("process.cards.3.title"),
+      text: t("process.cards.3.p"),
+      img: Developement,
+    },
+    {
+      title: t("process.cards.4.title"),
+      text: t("process.cards.4.p"),
+      img: Result,
+    },
+  ];
 
-	return (
-		<Swiper
-			className="mt-[15px]"
-			modules={[ Navigation, A11y ]}
-			navigation
-			width={700}
-			pagination={{ clickable: true }}
-			scrollbar={{ draggable: true }}
-			spaceBetween={0}
-			slidesPerView={1}
-			breakpoints={{
-				545: {
-					slidesPerView: 2
-				}
-			}}
-			onSlideChange={() => console.log('slide change')}
-			onSwiper={(swiper) => console.log(swiper)}
-		>
-			{slides.map(({ title, img, text }, index) => (
-				<SwiperSlide key={index}>
-					<SmallCards title={title} p={text} src={img} />
-				</SwiperSlide>
-			))}
-		</Swiper>
-	);
+  return (
+    <Swiper
+      className="mt-[15px] block md:hidden"
+      slidesPerView={1}
+      spaceBetween={30}
+      centeredSlides={true}
+      centeredSlidesBounds={true}
+      navigation={true}
+      modules={[Navigation]}
+      breakpoints={{
+        541: {
+          slidesPerView: 2,
+        },
+      }}
+    >
+      {slides.map(({ title, img, text }, index) => (
+        <SwiperSlide key={index}>
+          <SmallCards title={title} p={text} src={img} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 };
 
 export default Slider;
--- frontend/src/components/smallcards/index.tsx
+++ frontend/src/components/smallcards/index.tsx
@@ -1,40 +1,43 @@
-import {useTranslation} from "next-i18next";
-import Image from 'next/image';
-import Link from 'next/link';
-import {IoChevronForwardOutline} from 'react-icons/io5';
+import { useTranslation } from "next-i18next";
+import Image from "next/image";
+import Link from "next/link";
+import { IoChevronForwardOutline } from "react-icons/io5";
 import { FC } from "react";
 
-
-interface SmallCardsProps{
-    title: string;
-    p: string;
-    link?: string;
-    src: string;
+interface SmallCardsProps {
+  title: string;
+  p: string;
+  link?: string;
+  src: string;
 }
 
-const SmallCards:FC<SmallCardsProps> = ({
-    title,
-    p,
-    link,
-    src
-}) => {
-    const {t} = useTranslation("common");
+const SmallCards: FC<SmallCardsProps> = ({ title, p, link, src }) => {
+  const { t } = useTranslation("common");
 
   return (
-    <div className="max-w-[320px] max-h-[493px] rounded-3xl shadow-shadow p-6 mr-0">
-        <div className="flex flex-col items-center">
-            <h2 className="text-[29px] xxs:text-[40px] font-medium text-center text-transparent bg-clip-text bg-grad-text-2">{title}</h2>
-            <p className="text-center text-[14px] xxs:text-[16px] mt-2">{p}</p>
-          {link &&  <Link href="target">
-                <a className="mt-2 text-dark-pink flex items-center underline"> link <span ><IoChevronForwardOutline /></span>
-                </a>
-            </Link>}
-        </div>
-        <div className="mt-5 flex justify-center">
-            <Image src={src} />
-        </div>
+    <div className="max-w-[320px] max-h-[493px] rounded-3xl shadow-shadow p-6 mx-auto ">
+      <div className="flex flex-col items-center">
+        <h2 className="text-[29px] xxs:text-[40px] font-medium text-center text-transparent bg-clip-text bg-grad-text-2">
+          {title}
+        </h2>
+        <p className="text-center text-[14px] xxs:text-[16px] mt-2">{p}</p>
+        {link && (
+          <Link href="target">
+            <a className="mt-2 text-dark-pink flex items-center underline">
+              {" "}
+              link{" "}
+              <span>
+                <IoChevronForwardOutline />
+              </span>
+            </a>
+          </Link>
+        )}
+      </div>
+      <div className="mt-5 flex justify-center">
+        <Image src={src} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SmallCards
+export default SmallCards;
