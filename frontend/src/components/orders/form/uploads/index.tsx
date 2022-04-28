90d523d319b31447bbdcb6f89345ecf066ac1c4f
khadetou <khadetou96@gmail.com>
2022-04-28 17:41:29 +0000

    upload styling

--- frontend/src/components/orders/form/uploads/index.tsx
+++ frontend/src/components/orders/form/uploads/index.tsx
@@ -1,4 +1,6 @@
-import React, { FC } from "react";
+import uploadImg from "/public/images/upload.svg";
+import Image from "next/image";
+import { FC } from "react";
 
 interface UploadProps {
   onChange: (e: any) => void;
@@ -6,8 +8,34 @@ interface UploadProps {
 const Upload: FC<UploadProps> = ({ onChange }) => {
   return (
     <>
-      <label htmlFor="">Uplaod file</label>
-      <input type="file" name="design" multiple onChange={onChange} />
+      <div className="max-w-[422px] rounded-[35px] shadow-shadow-sm p-[33px]">
+        <label
+          htmlFor="upload"
+          className="flex cursor-pointer z-20 relative flex-col justify-center bg-light-gray rounded-[20px] h-[268px] focus:ring-0 border-none outline-none w-full"
+        >
+          <Image src={uploadImg} />
+          <span className="mx-auto">Uplaod files</span>
+          <p className="mx-auto text-center max-w-[255px] text-gray text-base mt-[8px]">
+            Designs must be <span className="text-dark">figma</span> or{" "}
+            <span className="text-black">sketch</span> or{" "}
+            <span className="text-black">adobe xd</span> or
+            <span className="text-black">pdf</span> format,
+            <span className="text-black"> Heigh resolution</span>
+            image.
+          </p>
+          <input
+            id="upload"
+            type="file"
+            name="design"
+            multiple
+            onChange={onChange}
+            className="opacity-0"
+          />
+          <div className="left-1/2 -translate-x-1/2 text-base font-bold px-[96.5px] text-white bg-dark-pink rounded-full py-[15px] absolute -bottom-4  ">
+            Upload
+          </div>
+        </label>
+      </div>
     </>
   );
 };
