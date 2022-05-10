235517a229e4953070614a0e7c58d2ddc0171acb
khadetou <khadetou96@gmail.com>
2022-05-10 09:58:27 +0000

    banner responsive termintated

--- frontend/src/screens/design/Banner.tsx
+++ frontend/src/screens/design/Banner.tsx
@@ -12,7 +12,7 @@ const Banner = () => {
     <section className="h-[890px] flex items-center bg-grad-back blend relative ">
       <div className="flex  flex-col items-center lg:flex-row translate-y-[-50%] md:mb-[110px] xsm:mb-[103px] lg:transform-none containers -mt-9">
         <div className="max-w-[734px] w-full mb-4 lg:mb-[-9.5rem]">
-          <h1 className="text-white  w-full text-[32px] leading-[1.2] xxs:text-[40px]  md:text-[40px] xs:text-[47px] lg:text-[64px]  2xl:text-[89px]  2xl:leading-none  font-light">
+          <h1 className="text-white  w-full text-[32px] leading-[1.2] xxs:text-[40px]  md:text-[40px] xs:text-[46px] lg:text-[64px]  2xl:text-[89px]  2xl:leading-none  font-light">
             We Create and build Incredible design
           </h1>
           <p className="text-white  xl:leading-snug text-[14px] leading-[1.2] xs:text-[18px] font-light text-lg mb-5 mt-[10px] sm:my-[10px] xl:font-medium  max-w-[635px]">
@@ -21,11 +21,11 @@ const Banner = () => {
             be profetionnal you are on the right place.
           </p>
           <div className="flex items-center  md:mt-6">
-            <button className="bg-white max-h-[50px] text-dark text-sm xxs:text-base py-2 px-4  xxs:px-10 xxs:py-[11px] font-bold rounded-full shadow-btn">
+            <button className="bg-white max-h-[50px] text-dark text-sm py-2 px-4 sm:text-base sm:px-10 sm:py-[11px] font-bold rounded-full shadow-btn">
               Start Now
             </button>
-            <div className="md:ml-6 ml-[7px]">
-              <Circular className="rounded-full p-[12px] xs:px-4 xs:py-4 xxs:p-[11px] bg-white text-primary shadow-btn lg:p-4" />
+            <div className="md:ml-6  xxs:ml-[13px] ml-[7px]">
+              <Circular className="rounded-full p-[12px] xxs:p-[11px] bg-white text-primary shadow-btn lg:p-4" />
               <span className="text-white xxs:text-[14px] md:text-base xs:text-[14px] font-normal ml-[5px] text-[10px] lg:text-lg lg:font-semibold uppercase">
                 Watch the video
               </span>
