bbafa3b4f02467e2198b367bfdebefbf17aba44c
khadetou <khadetou96@gmail.com>
2022-05-12 10:07:41 +0000

    card colors

--- frontend/src/screens/blogs/Cards.tsx
+++ frontend/src/screens/blogs/Cards.tsx
@@ -15,6 +15,23 @@ const Cards = () => {
         ))}
       </div>
       <Line />
+      <CatTitles className="bg-yellow-500" />
+      <div className="containers mb-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[30px]">
+        {Array.from({ length: 3 }).map((_, index) => (
+          <div key={index}>
+            <Card className="bg-yellow-500" />
+          </div>
+        ))}
+      </div>
+      <Line />
+      <CatTitles className="bg-blue-800" />
+      <div className="containers mb-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[30px]">
+        {Array.from({ length: 3 }).map((_, index) => (
+          <div key={index}>
+            <Card className="bg-blue-800" />
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
