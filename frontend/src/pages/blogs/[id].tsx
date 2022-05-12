e20ff184dcc08e19d991685f58c0924572925f24
khadetou <khadetou96@gmail.com>
2022-05-12 12:30:19 +0000

    blog page

--- frontend/src/pages/blogs/[id].tsx
+++ frontend/src/pages/blogs/[id].tsx
@@ -1,11 +1,12 @@
 import Header from "@/components/header";
+import Article from "@/screens/blogs/Article";
 import React from "react";
 
 const SingleBlog = () => {
   return (
     <div>
-      {" "}
-      <Header /> SingleBlog
+      <Header />
+      <Article />
     </div>
   );
 };
