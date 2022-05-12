e510cd18c54b6d73b199d9e3853c79719a769d7a
khadetou <khadetou96@gmail.com>
2022-05-12 08:09:34 +0000

    banner responsive

--- frontend/src/components/blogs/Title.tsx
+++ frontend/src/components/blogs/Title.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Title = () => {
   return (
     <h1
-      className={`lg:text-[86px] xxs:text-[47px] md:text-[60px] md:mt-[66px] leading-none text-[32px] mt-[32px] lg:leading-[104px] font-bold text-dark text-center lg:mt-[66px]  `}
+      className={`lg:text-[86px] xxs:text-[47px] md:text-[60px] md:mt-[66px] leading-[1.2] text-[32px] mt-[32px] lg:leading-[104px] font-bold text-dark text-center lg:mt-[66px]  `}
     >
       Hotcodes{" "}
       <span className="text-transparent bg-clip-text bg-grad-text-2">
--- frontend/src/screens/blogs/Index.tsx
+++ frontend/src/screens/blogs/Index.tsx
@@ -2,6 +2,8 @@ import Title from "@/components/blogs/Title";
 import Titlebold from "@/components/Title/titlebold";
 import { useState } from "react";
 import { BsSearch } from "react-icons/bs";
+import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
+import { FiInstagram, FiTwitter } from "react-icons/fi";
 
 const Banner = () => {
   const [search, setSearch] = useState("");
@@ -9,13 +11,15 @@ const Banner = () => {
     setSearch(e.target.value);
   };
 
+  const Socials = [FiInstagram, FiTwitter, FaFacebookF, FaLinkedinIn];
+
   return (
-    <section className="mb-[160px]">
+    <section className="my-[160px] h-full">
       <div className="containers flex flex-col items-center justify-center">
         <Title />
         <Titlebold
           title="“ Blog ”"
-          className="text-transparent bg-grad-text-2 bg-clip-text !mt-0"
+          className="text-transparent bg-grad-text-2 bg-clip-text !mt-4 !leading-[1.2] md:!mt-0"
         />
         <p className="font-medium mt-3 text-lg text-gray text-center">
           Have complete access to all the news about tech and entrepreneurship
@@ -26,7 +30,7 @@ const Banner = () => {
         >
           <label
             htmlFor="search"
-            className="absolute top-1/2 -translate-y-1/2 left-9"
+            className="absolute top-1/2 -translate-y-1/2 left-6 md:left-9"
           >
             <BsSearch className="text-dark" size="20" />
           </label>
@@ -37,16 +41,27 @@ const Banner = () => {
             name="search"
             id="search"
             placeholder="search"
-            className={`bg-[#f5f5f5] focus:border placeholder:text-base placeholder:xxs:text-[18px] placeholder:font-normal focus:border-[#e293d3] focus:ring-0 border-none focus:shadow-input focus:shadow-[#e9aede] w-full h-full rounded-[13px] outline-none px-[20px] md:px-[60px] py-2 text-dark text-lg md:text-[22px] font-medium  `}
+            className={`bg-[#f5f5f5] focus:border placeholder:text-base placeholder:xxs:text-[18px] placeholder:font-normal focus:border-[#e293d3] focus:ring-0 border-none focus:shadow-input focus:shadow-[#e9aede] w-full h-full rounded-[13px] outline-none px-[40px] md:px-[60px] py-2 text-dark text-lg md:text-[22px] font-medium  `}
           />
 
           <button
             type="button"
-            className="text-base font-bold text-white max-w-[96px] w-full h-[51px] rounded-[13px] bg-dark-pink absolute top-1/2 -translate-y-1/2 right-9"
+            className="text-sm md:text-base font-bold text-white max-w-[80px] md:max-w-[96px] w-full max-h-[47px] md:max-h-[51px] h-full rounded-[13px] bg-dark-pink absolute top-1/2 -translate-y-1/2 right-5 md:right-9"
           >
             GO
           </button>
         </label>
+
+        <div className="flex max-w-[381px] w-full justify-between mt-[30px]">
+          {Socials.map((Social, index) => (
+            <div
+              key={index}
+              className="w-full hover:shadow-shadow cursor-pointer flex justify-center items-center max-w-[60px] rounded-[16px] hover:text-dark-pink hover:scale-125 text-dark h-[60px] shadow-shadow-sm transition-all ease-in-out duration-700"
+            >
+              <Social size="25px" />
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
