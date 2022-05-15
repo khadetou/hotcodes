d49f99bf013d04f172011648a0c02c08840f2247
khadetou <khadetou96@gmail.com>
2022-05-15 12:39:44 +0000

    blog config page

--- frontend/src/screens/dashboard/blogScreen/index.tsx
+++ frontend/src/screens/dashboard/blogScreen/index.tsx
@@ -1,3 +1,5 @@
+import Buttons from "@/components/dashboard/blog/Buttons";
+import Cards from "@/components/dashboard/blog/cards";
 import React, { useState } from "react";
 import Header from "../header";
 import Sidebar from "../sidebar";
@@ -5,6 +7,25 @@ import WelcomeBanner from "../WelcomeBanner";
 
 const BlogScreen = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const buttons = [
+    {
+      text: "Developement",
+      color: "bg-blue-500",
+    },
+    {
+      text: "Design",
+      color: "bg-green-500",
+    },
+    {
+      text: "Marketing",
+      color: "bg-orange-500",
+    },
+    {
+      text: "Business",
+      color: "bg-purple-500",
+    },
+  ];
+
   return (
     <section className="flex h-screen overflow-hidden">
       <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
@@ -15,6 +36,11 @@ const BlogScreen = () => {
             <WelcomeBanner />
             <div className="sm:flex sm:justify-between sm:items-center mb-8">
               {/* Right: Actions */}
+              <div>
+                {buttons.map(({ text, color }, i) => (
+                  <Buttons text={text} className={color} key={i} />
+                ))}
+              </div>
 
               <button className="btn bg-pink-500 hover:bg-pink-600 !w-full max-w-[109px] h-[38px] rounded-md flex items-center justify-center text-white">
                 <svg
@@ -23,10 +49,14 @@ const BlogScreen = () => {
                 >
                   <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
                 </svg>
-                <span className="hidden xs:block ml-2">Add Fund</span>
+                <span className="hidden xs:block ml-2">New Blog</span>
               </button>
             </div>
-            <div className="grid grid-cols-12 gap-6"></div>
+            <div className="grid grid-cols-12 gap-6">
+              {Array.from({ length: 10 }).map((_, i) => (
+                <Cards key={i} />
+              ))}
+            </div>
           </div>
         </main>
       </div>
