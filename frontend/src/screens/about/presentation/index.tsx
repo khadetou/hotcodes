bc0f3077dccea98cfdc60519f59ab398d5cd948f
khadetou <khadetou96@gmail.com>
2022-05-16 10:40:27 +0000

    text-to-change

--- frontend/src/screens/about/presentation/index.tsx
+++ frontend/src/screens/about/presentation/index.tsx
@@ -8,25 +8,23 @@ import { useTranslation } from "next-i18next";
 import Field from "./field";
 
 const Presentation = () => {
-  const { t } = useTranslation("common");
+  const { t } = useTranslation("about");
 
   const presentations = [
     {
-      title: "WHO WE ARE",
-      h1: "Boots Your. Website Traffic !",
-      paragraph:
-        "We are passionate about our work. Our designers stay ahead curve to provide engaging and user-friendly website designs to make your business stand out. Our developers are committed to maintaining the highest web standards so that your site will withstand the test of time. We care about your business, wich is why we work with you",
-      button: t("presentation.design.button"),
+      title: "",
+      h1: t("presentation.aboutUs.h1"),
+      paragraph: t("presentation.aboutUs.p"),
+      button: "",
       src: WhoWeAre,
       style: "lg:flex-row-reverse",
       flip: "",
     },
     {
       title: "",
-      h1: "Our. Mission",
-      paragraph:
-        "We are passionate about our work. Our designers stay ahead curve to provide engaging and user-friendly website designs to make your business stand out. Our developers are committed to maintaining the highest web standards so that your site will withstand the test of time. We care about your business, wich is why we work with you",
-      button: t("presentation.dev.button"),
+      h1: t("presentation.mission.h1"),
+      paragraph: t("presentation.mission.p"),
+      button: "",
       src: Mission,
       style: "lg:flex-row",
       flip: "flip-y",
