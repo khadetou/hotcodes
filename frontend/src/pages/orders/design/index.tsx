93deb505c0aeb60b5cccea0e65f5eaca9ea2573f
khadetou <khadetou96@gmail.com>
2022-05-18 06:59:50 +0000

    card translation

--- frontend/src/pages/orders/design/index.tsx
+++ frontend/src/pages/orders/design/index.tsx
@@ -4,6 +4,8 @@ import { useActions } from "@/hooks/useActions";
 import Cards from "@/screens/design/Cards";
 import Header from "@/components/header";
 import Footer from "@/components/footer/Footer";
+import { GetServerSideProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 const Design = () => {
   const { CreateOrderWeb } = useActions();
@@ -19,3 +21,13 @@ const Design = () => {
 };
 
 export default Design;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { locale } = context;
+
+  return {
+    props: {
+      ...(await serverSideTranslations(locale!, ["about", "header"])),
+    },
+  };
+};
