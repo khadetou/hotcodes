e9fa7bfeb5331d6b13752b1a52a1a26f809e4b36
khadetou <khadetou96@gmail.com>
2022-05-26 20:19:53 +0000

    translation  implementation

--- frontend/src/components/orders/form/uploads/index.tsx
+++ frontend/src/components/orders/form/uploads/index.tsx
@@ -1,11 +1,13 @@
 import uploadImg from "/public/images/upload.svg";
 import Image from "next/image";
 import { FC } from "react";
+import { useTranslation } from "next-i18next";
 
 interface UploadProps {
   onChange: (e: any) => void;
 }
 const Upload: FC<UploadProps> = ({ onChange }) => {
+  const { t } = useTranslation("common");
   return (
     <>
       <div className="max-w-[422px] rounded-[35px] shadow-shadow-sm p-[33px]">
@@ -14,14 +16,15 @@ const Upload: FC<UploadProps> = ({ onChange }) => {
           className="flex cursor-pointer z-20 relative px-7 flex-col justify-center bg-light-gray rounded-[20px] h-[268px] focus:ring-0 border-none outline-none w-full"
         >
           <Image src={uploadImg} />
-          <span className="mx-auto">Uplaod files</span>
+          <span className="mx-auto">{t("Form.upload.title")}</span>
           <p className="mx-auto text-center max-w-[255px] text-gray text-base mt-[8px]">
-            Designs must be <span className="text-dark">figma</span> or{" "}
+            {/* Designs must be <span className="text-dark">figma</span> or{" "}
             <span className="text-black">sketch</span> or{" "}
             <span className="text-black">adobe xd</span> or
             <span className="text-black">pdf</span> format,
             <span className="text-black"> Heigh resolution</span>
-            image.
+            image. */}
+            {t("Form.upload.p")}
           </p>
           <input
             id="upload"
@@ -33,7 +36,7 @@ const Upload: FC<UploadProps> = ({ onChange }) => {
           />
 
           <div className="left-1/2 flex items-center justify-center -translate-x-1/2 text-base font-bold  text-white bg-dark-pink rounded-full w-full py-[15px] absolute -bottom-4  ">
-            Upload
+            {t("Form.upload.button")}
           </div>
         </label>
       </div>
