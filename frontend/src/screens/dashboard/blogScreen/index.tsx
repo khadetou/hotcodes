5f23b4ae3137b62b1a0c5d730f62ae982a931be1
khadetou <khadetou96@gmail.com>
2022-06-04 18:10:13 +0000

    blog check if is admin

--- frontend/src/screens/dashboard/blogScreen/index.tsx
+++ frontend/src/screens/dashboard/blogScreen/index.tsx
@@ -1,5 +1,6 @@
 import Buttons from "@/components/dashboard/blog/Buttons";
 import Cards from "@/components/dashboard/blog/Cards";
+import { useTypedSelector } from "@/hooks/useTypeSelector";
 import React, { useState } from "react";
 import Header from "../header";
 import Sidebar from "../sidebar";
@@ -26,6 +27,8 @@ const BlogScreen = () => {
     },
   ];
 
+  const { user } = useTypedSelector((state) => state.authReducer);
+
   return (
     <section className="flex h-screen overflow-hidden">
       <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
@@ -42,15 +45,17 @@ const BlogScreen = () => {
                 ))}
               </div>
 
-              <button className="btn bg-pink-500 hover:bg-pink-600 !w-full max-w-[109px] h-[38px] rounded-md flex items-center justify-center text-white">
-                <svg
-                  className="w-4 h-4 fill-current opacity-50 shrink-0"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
-                </svg>
-                <span className="hidden xs:block ml-2">New Blog</span>
-              </button>
+              {user?.roles.includes("admin") && (
+                <button className="btn bg-pink-500 hover:bg-pink-600 !w-full max-w-[109px] h-[38px] rounded-md flex items-center justify-center text-white">
+                  <svg
+                    className="w-4 h-4 fill-current opacity-50 shrink-0"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
+                  </svg>
+                  <span className="hidden xs:block ml-2">New Blog</span>
+                </button>
+              )}
             </div>
             <div className="grid grid-cols-12 gap-6">
               {Array.from({ length: 10 }).map((_, i) => (
